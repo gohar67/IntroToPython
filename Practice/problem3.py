02067343061601468e62322c5242cf54c2048236
gohar67 <gohar.aghajan@gmail.com>
2020-03-07 12:31:51 +0400

    problem 2-6

---
+++ Practice/problem3.py
@@ -0,0 +1,10 @@
+course = "Intro to Python"
+student = "Gohar Aghajanyan"
+Nickname = "Gohar93"
+Grade = 100
+python_expert = True
+print("course:" + course)
+print("student: " +student)
+print("nickname: " +Nickname)
+print("Grade", Grade)
+print("python_expert", python_expert)
---
+++ Practice/problem4.py
@@ -0,0 +1,5 @@
+AB = 3
+AC = 4
+BC = AB**2 + AC**2
+BC **=(1/2)
+print("the hypotenuse of the triangle ABC is ", BC)
