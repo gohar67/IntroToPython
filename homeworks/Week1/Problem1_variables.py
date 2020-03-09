6446a8d52b7875e56627bea3805c02fbd05f2db4
gohar67 <gohar.aghajan@gmail.com>
2020-03-10 00:33:05 +0400

    my first homework

---
+++ homeworks/Week1/Problem1_variables.py
@@ -0,0 +1,8 @@
+#Problem 1 ( Variables )
+project = "cake"
+difficulty = 5
+ingredients = ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print("apple" in ingredients)
+print("butter" in ingredients)
+print(("eggs" in ingredients) or ("margarine" in ingredients))
+print(("eggs" in ingredients) and ("margarine" in ingredients))
