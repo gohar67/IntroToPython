859b086fd2d23ef9d4633509f99ddaf78343e8fc
gohar67 <gohar.aghajan@gmail.com>
2020-03-10 00:43:05 +0400

    my 2nd homework

--- homeworks/Week1/Problem1_variables.py
+++ homeworks/Week1/Problem1_variables.py
@@ -6,3 +6,22 @@ print("apple" in ingredients)
 print("butter" in ingredients)
 print(("eggs" in ingredients) or ("margarine" in ingredients))
 print(("eggs" in ingredients) and ("margarine" in ingredients))
+
+flour = 175
+butter = 175
+sugar = "100g"
+eggs = 2
+cocoa_powder = "1ts"
+baking_powder = 0.5
+print(type(flour))
+print(type(butter))
+print(type(sugar))
+print(type(eggs))
+print(type(cocoa_powder))
+print(type(baking_powder))
+print("flour" + " -", flour)
+print("butter" + " -", butter)
+print("sugar" + " - " + sugar)
+print("eggs" + " -", eggs)
+print("cocoa powder" + " - ", cocoa_powder)
+print("baking powder" + " -", baking_powder)
